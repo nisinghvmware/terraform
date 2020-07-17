84c236853cda68f3f8230947b7d0fd5b08f03fb0
Prashant <prashantgadagi88@gmail.com>
2020-07-17 14:22:07 -0700

    Update main.tf

--- aws-with-volume/main.tf
+++ aws-with-volume/main.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-    region="us-west-1"
+    region="us-east-1"
 }
 
 variable "instance_type" {
@@ -13,12 +13,12 @@ resource "aws_volume_attachment" "ebs_att" {
   instance_id = "${aws_instance.machine1.id}"
 }
 resource "aws_ebs_volume" "volume1" {
-  availability_zone = "us-west-1a"
+  availability_zone = "us-east-1a"
   size              = 1
 }
 
 resource "aws_instance" "machine1" {
     ami           = "ami-0b91a410940e82c54"
     instance_type = "t2.micro"
-    availability_zone = "us-west-1a"    
+    availability_zone = "us-east-1a"    
 }
