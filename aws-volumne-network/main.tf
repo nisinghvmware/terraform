0ecfd9216800731e81f3c7c2a8ad3bcc58afe560
Prashant Gadagi <gadagip@vmware.com>
2020-08-04 13:19:31 -0700

    Update main.tf

--- aws-volumne-network/main.tf
+++ aws-volumne-network/main.tf
@@ -1,26 +1,12 @@
 provider "aws" {
     region="us-east-1"
+    access_key = "AKIAJ7LYZ732FOJUCR2Q"
+    secret_key = "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"
 }
 
 variable "instance_type" {
   description = "AWS instance type"
-  default     = "t2.micro"
-}
-
-resource "aws_volume_attachment" "ebs_att" {
-  device_name = "/dev/sdh"
-  volume_id   = aws_ebs_volume.volume1.id
-  instance_id = aws_instance.machine1.id
-}
-resource "aws_ebs_volume" "volume1" {
-  availability_zone = "us-east-1a"
-  size              = 1
-}
-
-resource "aws_instance" "machine1" {
-    ami           = "ami-04b9e92b5572fa0d1"
-    instance_type = "t2.micro"
-    availability_zone = "us-east-1a"    
+  default     = "t2.nano"
 }
 
 resource "aws_vpc" "main" {
@@ -28,6 +14,7 @@ resource "aws_vpc" "main" {
 }
 
 resource "aws_subnet" "main" {
+  availability_zone = "us-east-1a"
   vpc_id     = aws_vpc.main.id
   cidr_block = "10.0.0.0/24"
   tags = {
@@ -60,14 +47,31 @@ resource "aws_security_group" "allow_tls" {
   }
 }
 
-resource "aws_network_interface" "test" {
+resource "aws_volume_attachment" "ebs_att" {
+  device_name = "/dev/sdh"
+  volume_id   = aws_ebs_volume.volume1.id
+  instance_id = aws_instance.machine1.id
+}
+resource "aws_ebs_volume" "volume1" {
+  availability_zone = "us-east-1a"
+  size              = 1
+}
+
+resource "aws_network_interface_attachment" "net_att" {
+  instance_id          = aws_instance.machine1.id
+  network_interface_id = aws_network_interface.network1.id
+  device_index         = 1
+}
+
+resource "aws_network_interface" "network1" {
   subnet_id       = aws_subnet.main.id
   private_ips     = ["10.0.0.15"]
   security_groups = [aws_security_group.allow_tls.id]
-  availability_zone = "us-east-1a"
+}
 
-  attachment {
-    instance     = aws_instance.machine1.id
-    device_index = 1
-  }
+resource "aws_instance" "machine1" {
+    ami           = "ami-04b9e92b5572fa0d1"
+    instance_type = "t2.micro"
+    availability_zone = "us-east-1a"
+    subnet_id = aws_subnet.main.id
 }
